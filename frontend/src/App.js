165b449db5c34533ed9c3d93da4cc1f1167608db
its-muhammed <muhammed.20210923@iit.ac.lk>
2025-01-22 08:32:38 +0530

    Initialize React in frontend and update homepage

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Freelancing Platform</h1>
+      <p>Welcome to our decentralized freelancing marketplace!</p>
+    </div>
+  );
+}
+
+export default App;
